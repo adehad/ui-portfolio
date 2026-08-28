249e817d49b024de3c40d19aa20b6a735ef6393f
adehad <26027314+adehad@users.noreply.github.com>
2026-08-28 11:19:10 +0100

    chore: storybook 9 on react-vite

---
+++ .storybook/preview.ts
@@ -0,0 +1,11 @@
+import type { Preview } from "@storybook/react-vite";
+import "../src/styles/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: { matchers: { color: /(background|color)$/i, date: /Date$/i } },
+    layout: "fullscreen",
+  },
+};
+
+export default preview;
